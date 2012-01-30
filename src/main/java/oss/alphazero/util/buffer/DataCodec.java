feade589613fdb60a5b45324d9ae2440e1193a80
Joubin Houshyar <alphazero@sensesay.net>
2012-01-30 01:25:52 -0500

    Fix int(v, b, o)  and update DataCode to handle longs.

--- src/main/java/oss/alphazero/util/buffer/DataCodec.java
+++ src/main/java/oss/alphazero/util/buffer/DataCodec.java
@@ -1,6 +1,5 @@
 package oss.alphazero.util.buffer;
 
-import java.io.IOException;
 
 /**
  * Convert to and from language primitives from provided buffers.
@@ -9,28 +8,43 @@ import java.io.IOException;
  */
 public class DataCodec {
 	static public final int LONG_BYTES = Long.SIZE / Byte.SIZE;
-	static public final byte[] writeLong(final long v, byte[] b) {
+	static public final int INTEGER_BYTES = Integer.SIZE / Byte.SIZE;
+	static public final byte[] writeLong(final long v, final byte[] b) {
 	    return writeLong(v, b, 0);
 	}
-	static public final byte[] writeLong(final long v, byte[] b, int off) {
+	static public final byte[] writeLong(final long v, final byte[] b, int off) {
 		if(b==null)
 			throw new NullPointerException("b");
 		if(b.length - off < LONG_BYTES) 
 			throw new IllegalArgumentException(String.format("(b.len:%d, off:%d)", b.length, off).toString());
-	    b[0] = (byte)(v >>> 56);
-	    b[1] = (byte)(v >>> 48);
-	    b[2] = (byte)(v >>> 40);
-	    b[3] = (byte)(v >>> 32);
-	    b[4] = (byte)(v >>> 24);
-	    b[5] = (byte)(v >>> 16);
-	    b[6] = (byte)(v >>>  8);
-	    b[7] = (byte)(v >>>  0);
+	    b[off]   = (byte)(v >>> 56);
+	    b[off+1] = (byte)(v >>> 48);
+	    b[off+2] = (byte)(v >>> 40);
+	    b[off+3] = (byte)(v >>> 32);
+	    b[off+4] = (byte)(v >>> 24);
+	    b[off+5] = (byte)(v >>> 16);
+	    b[off+6] = (byte)(v >>>  8);
+	    b[off+7] = (byte)(v >>>  0);
 	    return b;
 	}
-	static public final long readLong(byte[] b) throws IOException {
+	static public final byte[] writeInt(final int v, final byte[] b){
+		return writeInt(v, b, 0);
+	}
+	static public final byte[] writeInt(final int v, final byte[] b, int off){
+		if(b==null)
+			throw new NullPointerException("b");
+		if(b.length - off < INTEGER_BYTES) 
+			throw new IllegalArgumentException(String.format("(b.len:%d, off:%d)", b.length, off).toString());
+        b[off]   = (byte) ((v >>> 24) & 0xFF);
+        b[off+1] = (byte) ((v >>> 16) & 0xFF);
+        b[off+2] = (byte) ((v >>>  8) & 0xFF);
+        b[off+3] = (byte) ((v >>>  0) & 0xFF);
+        return b;
+	}
+	static public final long readLong(byte[] b){
 		return readLong(b, 0);
 	}
-	static public final long readLong(byte[] b, int off) throws IOException {
+	static public final long readLong(byte[] b, int off){
 		if(b==null)
 			throw new NullPointerException("b");
 		if(b.length - off < LONG_BYTES) 
